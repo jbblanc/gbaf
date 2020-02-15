1c1410e7334874c807a49a51a3c35fc7e5c76c68
Abde <abdenour.bensouna@rocketstud.io>
2020-02-15 20:24:14 +0100

    init add index.php section_acteurs.php and style/

---
+++ index.php
@@ -0,0 +1,36 @@
+<!doctype html>
+<html>
+    <head>
+        <title>GBAF extranet</title>
+        <meta charset="utf-8">
+    </head>
+    
+    <body>
+        <header>
+            <section>"logo GBAF"</section>
+            <section>$user_NAME&PREMON</section>
+        </header>
+
+        <main>
+            <div class="presentation">
+            <article>
+                <section>
+                    <h1>texte de présentation du GBAF et du site</h1>
+                    <img scr="" rel="illustration">
+                </section>
+                <section>
+                    <h2> text acteurs et partenaires</h2>
+                </section>
+            </div>
+           <?php include("sectin_acteurs.php"); ?>
+            </article>        
+        </main>
+
+        <footer>
+            <nav>
+                <p>| Mention légales | Contacts |</p>
+            </nav>
+        </footer>    
+    </body>
+    
+</html>
---
+++ section_acteurs.php
@@ -0,0 +1,6 @@
+<article class="zone_acteurs">
+    <article class="acteur">
+        <h3> contenu textuel ... + lien</h3>
+        <p>lire la suite</p>
+    </article>
+</article>
