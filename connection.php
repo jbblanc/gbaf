2c1f2356b221247d44edcbeb457ae7cc831ea49b
Abde <abdenour.bensouna@rocketstud.io>
2020-02-17 11:44:33 +0100

    mention legal add

--- connection.php
+++ connection.php
@@ -1,5 +1,5 @@
 <form name="connection" method="post" action="saisie.php">
         Entrez votre pseudo : <input type="text" name="pseudo"/> <br/>
-        Entrez votre modt de pass : <input type="password" name="password"/><br/>
+        Entrez votre mot de pass : <input type="password" name="password"/><br/>
         <input type="submit" name="valider" value="VALIDER"/>
-    </form>
+</form>
