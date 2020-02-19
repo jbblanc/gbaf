3ece325d96941645c3b952fd377055165438c5f4
Abde <abdenour.bensouna@rocketstud.io>
2020-02-19 13:25:10 +0100

    footer add

---
+++ footer.php
@@ -0,0 +1,11 @@
+<!Doctype html>
+<html>
+    <footer>
+            <nav class="nav_footer">
+                <p>
+                    <a href="./mentions_legales.php" target="_blank">| Mention légales |</a> 
+                    <a href="./contact.php" target="_blank">Contact </a> 
+                </p>
+            </nav>
+        </footer>
+</html>
