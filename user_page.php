19aaa3a4ca3d7276edb764ee39eeea7b7a2f79ce
Abde <abdenour.bensouna@rocketstud.io>
2020-02-19 18:56:05 +0100

    full page in if of connection

--- user_page.php
+++ user_page.php
@@ -6,23 +6,22 @@
     </head>
     
     <body>
-    <h1>Mon Compte</h1>
-    <p>
-        <?php
-        if (isset($_POST['UserName']) AND isset($_POST['Password']))
-            echo '<p>Bonjour ' . htmlspecialchars($_POST['UserName']) . ' '. 'Pass=' . htmlspecialchars($_POST['Password']) . '</p>';
-        else
-            echo 'Connectez-vous pour voir votre profile !';
-        ?>
-    </p>
     <?php
+    $connection = true;
     if ($connection === true)
         {
             echo '<a href="./index.php"><button>Se deconnecter</button></a>';
+            echo '<h1>Mon Compte</h1>';
+            if (isset($_POST['UserName']) AND isset($_POST['Password']))
+                echo '<p>Bonjour ' . htmlspecialchars($_POST['UserName']) . ' '. 'Pass=' . htmlspecialchars($_POST['Password']) . '</p>';
+            else
+                echo 'Connectez-vous pour voir votre profile !';
+        
         }
-    else
-        echo '<a href="./connection.php"><button>Se connecter</button></a>';
-            ?>
+        else
+            echo '<a href="./connection.php"><button>Se connecter</button></a>';
+        
+    ?>
         <footer>
             <?php include('./footer.php');?>
         </footer>
