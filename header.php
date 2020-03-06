dc718591ba11bd71e6c22cf90480e5f6858ce94f
Abde <abdenour.bensouna@rocketstud.io>
2020-03-06 20:33:46 +0100

    fix header

--- header.php
+++ header.php
@@ -1,11 +1,9 @@
-<div class="nav_header">
+<div class="">
     <a href="./home.php">
         <img id="img_gbaf" 
         src="https://user.oc-static.com/upload/2019/07/15/15631755744257_LOGO_GBAF_ROUGE%20%281%29.png"
         alt="Le logo de la GBAF">
     </a>
-    <a href="./user_page.php"><button>Paramètres du compte</button></a>
-    <a href='./sign_up.php'><button>Sign up</button></a>
-    <a href="./sign_in.php"><button>Sign in</button></a>
+    <a href="./user_page.php"><?=$_SESSION['nom']?> & <?=$_SESSION['prenom']?></a>
     <a href="./log_out.php"><button>Log out</button></a>
 </div>
