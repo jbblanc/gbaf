54b0b7ee73de28bd868d1a616461640d1aa3cf9c
Abde <abdenour.bensouna@rocketstud.io>
2020-03-06 16:35:45 +0100

    fix log out

--- log_out.php
+++ log_out.php
@@ -1,6 +1,13 @@
 <?php
-    session_start();
-    session_destroy();
-    header('Location: ./index.php');
-    exit();
+// On démarre la session
+session_start ();
+
+// On détruit les variables de notre session
+session_unset ();
+
+// On détruit notre session
+session_destroy ();
+
+// On redirige le visiteur vers la page d'accueil
+header('location: ./index.php');
 ?>
