6c4f0d15ad060198cf72eedf9982d6bd7f3d9fa2
Abde <abdenour.bensouna@rocketstud.io>
2020-03-07 18:37:45 +0100

    fix sct acts

--- section_acteurs.php
+++ section_acteurs.php
@@ -1,38 +1,20 @@
-<?php 
-    // if (i)
-    // {
+<?php
+require './_db.php';
 
-    // }
+//fetch of all acteurs
+$req = $pdo->query("SELECT * FROM acteurs");
+$acteurs = $req->fetchALL();
+
+//creat all divs fot actors
+
+foreach($acteurs as $acteur)
+{
 ?>
-<div class="zone_acteurs">
-    <article class="acteur">
-        <img>
-        <h3> contenu textuel ... + lien</h3>
-        <p>lire la suite</p>
-    </article>
-    <article class="acteur">
-        <img>
-        <h3> contenu textuel ... + lien</h3>
-        <p>lire la suite</p>
-    </article>
-    <article class="acteur">
-        <img>
-        <h3> contenu textuel ... + lien</h3>
-        <p>lire la suite</p>
-    </article>
-    <article class="acteur">
-        <img>
-        <h3> contenu textuel ... + lien</h3>
-        <p>lire la suite</p>
-    </article>
-    <article class="acteur">
-        <img>
-        <h3> contenu textuel ... + lien</h3>
-        <p>lire la suite</p>
-    </article>
-    <article class="acteur">
-        <img>
-        <h3> contenu textuel ... + lien</h3>
-        <p>lire la suite</p>
-    </article>
-</div>
+    <div class="acteur">
+        <img class="img_acteurs" src="<?=$acteur['acteur_picture_path']?>" rel="<?=$acteur['acteur_name']?>">
+        <h3><?=$acteur['text_contenu']?></h3></br>
+        <button><a href="./detail_acteur.php?id=<?=$acteur['id']?>">lire la suite</a></button>
+    </div></br>
+<?php
+}
+?>
