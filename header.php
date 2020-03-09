ee9ced1eb0bbb55cd003b6d80fb57f57c383ecbe
Abde <abdenour.bensouna@rocketstud.io>
2020-03-09 17:25:51 +0100

    header + se deconnecter

--- header.php
+++ header.php
@@ -1,9 +1,9 @@
 <div class="">
     <a href="./home.php">
-        <img id="img_gbaf" 
+        <img id="img_gbaf" style="width: 10%; heigth: 10%"
         src="https://user.oc-static.com/upload/2019/07/15/15631755744257_LOGO_GBAF_ROUGE%20%281%29.png"
         alt="Le logo de la GBAF">
     </a>
     <a href="./user_page.php"><?=$_SESSION['nom']?> & <?=$_SESSION['prenom']?></a>
-    <a href="./log_out.php"><button>Log out</button></a>
+    <a href="./log_out.php"><button>Se déconnecter</button></a>
 </div>
