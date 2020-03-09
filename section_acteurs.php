570579024e3b5e1fa0ba65613c759e0e08e164dd
Abde <abdenour.bensouna@rocketstud.io>
2020-03-09 19:29:37 +0100

    fix section acteur + style img

--- section_acteurs.php
+++ section_acteurs.php
@@ -4,14 +4,15 @@ require './_db.php';
 //fetch of all acteurs
 $req = $pdo->query("SELECT * FROM acteurs");
 $acteurs = $req->fetchALL();
-
+$req->closeCursor();
 //creat all divs fot actors
 
 foreach($acteurs as $acteur)
 {
 ?>
     <div class="acteur">
-        <img class="img_acteurs" src="<?=$acteur['acteur_picture_path']?>" rel="<?=$acteur['acteur_name']?>">
+        <img class="img_acteurs" style="width: 35%; heigth: 30%"
+        src="<?=$acteur['acteur_picture_path']?>" rel="<?=$acteur['acteur_name']?>">
         <h3><?=$acteur['text_contenu']?></h3></br>
         <button><a href="./detail_acteur.php?id=<?=$acteur['id']?>">lire la suite</a></button>
     </div></br>
