d5dfc876d04a2efabd078820f88579d24a1d9419
Abde <abdenour.bensouna@rocketstud.io>
2020-03-10 16:57:06 +0100

    modification db id to acteur id line 17

--- section_acteurs.php
+++ section_acteurs.php
@@ -14,7 +14,7 @@ foreach($acteurs as $acteur)
         <img class="img_acteurs" style="width: 35%; heigth: 30%"
         src="<?=$acteur['acteur_picture_path']?>" rel="<?=$acteur['acteur_name']?>">
         <h3><?=$acteur['text_contenu']?></h3></br>
-        <button><a href="./detail_acteur.php?id=<?=$acteur['id']?>">lire la suite</a></button>
+        <button><a href="./detail_acteur.php?id=<?=$acteur['acteur_id']?>">lire la suite</a></button>
     </div></br>
 <?php
 }
