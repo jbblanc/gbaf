48402f4302661f9d7a5244784c435da68959d658
Abde <abdenour.bensouna@rocketstud.io>
2020-03-11 17:49:56 +0100

    rm doctype.php

--- doctype.php
+++
@@ -1,8 +0,0 @@
-<!Doctype html>
-<html>
-    <head>
-        <meta charset="utf-8" />
-        <meta name="viewport" content="width=device-width, initial-scale=1, user-scalable=no">
-        <link href="style/style.css" rel="stylesheet">
-    </head>
-</html>
